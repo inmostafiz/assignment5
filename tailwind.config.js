4d9203987da7d5d8c6af5c664afa113a7a4122fc
inmostafiz <inmostafiz@gmail.com>
2025-08-29 09:59:38 +0600

    add nav and hero section

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        hind: ["Hind Madurai", "sans-serif"], // font-hind
+        inter: ["Inter", "sans-serif"],       // font-inter
+      },
+    },
+  },
+  plugins: [],
+};
